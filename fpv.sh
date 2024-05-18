1ca802e99993f267e09a04f1433457722b1f33d7
snokvist <joakim.snokvist@gmail.com>
2024-05-18 18:29:51 +0200

    Update fpv.sh

--- fpv.sh
+++ fpv.sh
@@ -24,7 +24,7 @@ sudo systemctl start wifibroadcast@gs
 #Restart gstreamer if it crashes.
 while true
 do
-gst-launch-1.0 udpsrc port=5600 ! tee name=t t. ! queue ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=25 ! rtph265depay ! vaapih265dec ! videoconvert ! videoscale ! videorate ! video/x-raw,framerate=144/1 ! fpsdisplaysink fps-update-interval=200 video-sink=xvimagesink sync=false t. ! queue leaky=1 ! application/x-rtp, payload=98, encoding-name=OPUS ! rtpjitterbuffer latency=25 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! jackaudiosink low-latency=true sync=false async=false
+gst-launch-1.0 udpsrc port=5600 ! tee name=t t. ! queue ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=1 ! rtph265depay ! vaapih265dec ! videoconvert ! videoscale ! videorate ! video/x-raw,framerate=144/1 ! fpsdisplaysink fps-update-interval=200 video-sink=xvimagesink sync=false t. ! queue leaky=1 ! application/x-rtp, payload=98, encoding-name=OPUS ! rtpjitterbuffer latency=1 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! jackaudiosink low-latency=true sync=false async=false
 echo "GST crashed with exit code:$?" >> fpv.log
 sleep 1
 done
