ee4fe41724c74af0757d603b41936c0aae8f119e
snokvist <joakim.snokvist@gmail.com>
2024-06-30 20:04:16 +0200

    Update fpv.sh

--- fpv.sh
+++ fpv.sh
@@ -9,9 +9,15 @@ onExit() {
     # sleep 0.1
 
     sudo modprobe -r rtw88_8822ce
+    #sudo modprobe -r 88XXau_wfb
+    #sudo modprobe -r 88x2bu
+    #sudo modprobe -r 8812eu
     # sleep 0.1
 
     sudo modprobe rtw88_8822ce
+    #sudo modprobe 88XXau_wfb
+    #sudo modprobe 88x2bu
+    #sudo modprobe 8812eu
 }
 
 trap "onExit" EXIT
@@ -19,12 +25,19 @@ trap "exit 2" HUP INT QUIT TERM
 
 sudo systemctl stop wifibroadcast@gs
 
+#Pull all available NICS to WFB_NG
+echo 'WFB_NICS="'"$(ifconfig | grep -o '\bwlan\w*' | paste -s -d ' ')"'"' | sudo tee /etc/default/wifibroadcast
+
 sudo systemctl start wifibroadcast@gs
 
 #Restart gstreamer if it crashes.
 while true
 do
-gst-launch-1.0 udpsrc port=5600 ! tee name=t t. ! queue ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=1 ! rtph265depay ! vaapih265dec ! videoconvert ! videoscale ! videorate ! video/x-raw,framerate=144/1 ! fpsdisplaysink fps-update-interval=200 video-sink=xvimagesink sync=false t. ! queue leaky=1 ! application/x-rtp, payload=98, encoding-name=OPUS ! rtpjitterbuffer latency=1 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! jackaudiosink low-latency=true sync=false async=false
+#Only video
+gst-launch-1.0 udpsrc port=5600 ! queue max-size-time=2600000 ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer drop-on-latency=true latency=26 do-lost=true ! rtph265depay ! vaapih265dec ! videoconvert ! videoscale ! videorate max-rate=100 average-period=100 ! fpsdisplaysink video-sink=xvimagesink sync=false
+
+#Vido + Audio
+#gst-launch-1.0 udpsrc port=5600 ! tee name=t t. ! queue max-size-time=2600000 ! application/x-rtp,payload=97, clock-rate=90000, encoding-name=H265 ! rtpjitterbuffer latency=26 ! rtph265depay ! vaapih265dec ! xvimagesink async=false sync=false t. ! queue leaky=1 ! application/x-rtp, payload=98, encoding-name=OPUS ! rtpjitterbuffer latency=10 ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! jackaudiosink low-latency=true sync=false async=false
 echo "GST crashed with exit code:$?" >> fpv.log
 sleep 1
 done
